c646f5cc8b2ed3981d8214ff1b9cc18e485e1247
Tarun Yadav <tarunyadav.iitp@gmail.com>
2012-03-13 01:25:41 +0530

    hough transformation done, line finding remaining

--- Assignment_7/hough_transformation.py
+++ Assignment_7/hough_transformation.py
@@ -1,7 +1,7 @@
 ## 
  #          File: frequency_filters.py
  #          Author: Tarun Yadav
- #          Last Modified: March 10, 2012
+ #          Last Modified: March 13, 2012
  #          Topic: Hough transformation for finding straight line
  #          Instructor: Dr. Anil Seth
  # -----------------------------------------------------------------------------------------------------------------------------------
@@ -19,18 +19,21 @@ from Image import *
 from pylab import *
 import numpy
 import scipy.ndimage.filters as filters
+import ImageDraw
 
 # open the image which is passed in argument
 def init():
+	global image
+	global image_rgb
 	global image_array
 	global image_output
 	global size
 	
-	image = open(sys.argv[1])  
-	image = image.convert('L')
+	image_rgb = open(sys.argv[1])  
+	image = image_rgb.convert('L')
 	image_array = array(image);
 	size = image_array.shape
-	image_array = are brightest in their neighborhood (here 10 * 10 pixels)ray(image_array,dtype=float)
+	image_array = array(image_array,dtype=float)
 	image_output = zeros((size[0],size[1]),dtype=float)
 	
 	
@@ -59,50 +62,32 @@ def SaveToFile(output_name):
 	image_mod.show()
 	
 #Function to roberts operator for edge detection	
-def robert():
+def roberts():
 	global image_array
 	image_arr = zeros((size[0],size[1]),dtype=float)
 	for i in range(0,size[0]-1,1):
 		 for j in range(0,size[1]-1,1):
 				image_arr[i][j] =  abs(image_array[i][j]-image_array[i+1][j+1])+abs(image_array[i][j+1]-image_array[i+1][j])
-										
-	image_arr = image_arr.clip(0,255)							
+	mask = (image_arr>128)
+	image_arr[mask]=255			
 	return image_arr
 			    			
 #Function to apply hough transformation
 #@paramter: steps for theta and r
 #@return: Hough array containg votes in r-theta space
 def Hough_Transformation(edge_image_array,delta_theta,delta_r):
-#	global image_array
 	
-	thetas = range(-90,90+round(delta_theta),round(delta_theta))
-	rs = range(0,sqrt(size[0]*size[0]+size[1]*size[1])+delta_r, delta_r )
-	Hough = zeros((len(rs),len(thetas)),dtype=float)
-	
-#	edge_image_flat= edge_image_array.flat 
-#	x  = range(0,size[1])
-#	y = range(0,size[0])
-#	y= repeat(y,size[1])
-#	int i=0;
-#	while (i<size[0]):
-#		x = append(x,x)
-#		i++
-#	thetas = range(-90,90+round(delta_theta),round(delta_theta))
-#	sine = sin(thetas*pi/180))
-#	cosine = cos(thetas*pi/180)
-#	r = transpose(edge_image_flat*x)*cosines + transpose(edge_image_array*y)*sines
-#	Hough = zeros((len(thetas),len(r)),dtype=float)
-#	for x in thetas:
-#			Hough[x][round(r[:][x])] = Hough[x][round(r[:][x])]+1
+	thetas = range(0,180+int(round(delta_theta)),int(round(delta_theta)))
+	max_rs=int(round(sqrt(size[0]*size[0]+size[1]*size[1])))
+	Hough = zeros((2*max_rs,len(thetas)),dtype=float)
 
 	for i in range(0,size[0]):
 		for j in range(0,size[1]):
-			if edge_image_array[i][j]==0:
-				continue
-			else:
+			if edge_image_array[i][j]==255:
 				for theta in thetas:
 					r = i*cos(theta*pi/180) + j*sin(theta*pi/180)
-					Hough[r][theta]++;
+					Hough[(r + max_rs)/delta_r][theta/delta_theta]=Hough[(r + max_rs)/delta_r][theta/delta_theta]+1;
+					
 	return Hough
 			    		
 #Function to detect local maximas in given r-theta space array
@@ -110,39 +95,56 @@ def Hough_Transformation(edge_image_array,delta_theta,delta_r):
 #@return : points of maximas in r-theta space, bool r-theta space matrix where all local maximas are true
 def Maxima_Detection(Hough,neighborhood):
 	
-	maximas = filters.maximun_filters(Hough,5)
-	local_maximas = (Hough==maximas)
-	
+	#maximas = filters.maximum_filter(Hough,neighborhood)
+	#local_maximas = (Hough==maximas)
+	mask = (Hough > 80)
+	hough_size = Hough.shape
+	local_maximas = zeros((hough_size[0],hough_size[1]),dtype=bool)
+	local_maximas[mask]=True
 	return local_maximas
 
 #Function to draw the straigt lines of maxima on the image
 #@parameter: matrix of r-theta space where all local maximas are true
 #@return: No return just draw all the straight lines got from hough transformations
-def Draw(maximas):
-	global image
+def Draw(maximas,delta_theta,delta_r):
+	global image_rgb
 	global image_output
 	
 	maximas_size = maximas.shape
-	draw = ImageDraw.Draw(image)
+	print(maximas_size)
+	count = 0
+	for r in range(0,maximas_size[0]):
+		for theta in range(0,maximas_size[1]):
+			if (maximas[r][theta] == True  ):
+				count = count +1
+	print count
+	draw = ImageDraw.Draw(image_rgb)
 	start_x=0;start_y=0
 	end_x=0;end_y=0
 	for r in range(0,maximas_size[0]):
+		r1=(r*int(round(delta_r)) -int(maximas_size[0]/2) )
 		for theta in range(0,maximas_size[1]):
-			if (maximas[r][theta] == TRUE):
-				if ((r/sin(theta)>size[0]-1):
+			theta1=theta*int(round(delta_theta))*pi/180
+			if (maximas[r][theta] == True  ):
+
+				if (r1/sin(theta1)>size[0]-1):
 					start_y = size[0]-1
-					start_x = r-(start_y*sin(theta))
+					start_x = (r1-(start_y*sin(theta1)))/cos(theta1)
 				else:
-					start_y = r/sin(theta)
+					start_y = r1/sin(theta1)
 					start_x = 0
-				if ((r/cos(theta)>size[1]-1):
+				if (r1/cos(theta1)>size[1]-1):
 					end_x = size[1]-1
-					end_y = r-(start_x*cos(theta))
+					end_y = (r1-(start_x*cos(theta1)))/sin(theta1)
 				else:
-					end_x = r/cos(theta)
+					end_x = r1/cos(theta1)
 					end_y = 0
-				draw.line([(start_x,start_y),(end_x,end_y)])
-	image_output = array(image)
+				draw.line([(start_x,start_y),(end_x,end_y)],fill=255)
+				#print(r,theta)
+	draw.line([(0,0),(100,100)],fill=255)				
+	print("tarun")
+	image_output = array(image_rgb)
+	print("tarun")
 	print "\nApplying Hough Transformation   "+"...Done\n"
 	output_name = '_hough_transformation_'
 	SaveToFile(output_name)
@@ -156,7 +158,7 @@ def user_input_fun():
 	if(delta_theta=="" or delta_theta=="" or neighborhood==""):
 		exit()
 	else:
-		return float(delta_theta),float(delta_r),neighborhood
+		return float(delta_theta),float(delta_r),int(neighborhood)
 
 		
 #main function which will be run 
@@ -168,12 +170,31 @@ def main():
 	(delta_theta,delta_r,neighborhood) = user_input_fun()
 	edge_image_array = roberts()
 	Hough = Hough_Transformation(edge_image_array,delta_theta,delta_r)
-	maximas = Maxima_Detection(Hough,neighborhood)
-	Draw(maximas);	
+	print "\nApplying Hough Transformation   "+"...Done\n"
+	output_name = '_hough_transformation_'
+	SaveToFile(output_name)
+	
+	#maximas = Maxima_Detection(Hough,neighborhood)
+	#Draw(maximas,delta_theta,delta_r);	
 	rerun = raw_input("\nPress 'q' to quit  : \n"+"Press Enter to run again: \n")
 	if(rerun==""):
 		main()
 		
 main()
 	
-	
+#	edge_image_flat= edge_image_array.flat 
+#	x  = range(0,size[1])
+#	y = range(0,size[0])
+#	y= repeat(y,size[1])
+#	int i=0;
+#	while (i<size[0]):
+#		x = append(x,x)
+#		i++
+#	thetas = range(0,180+round(delta_theta),round(delta_theta))
+#	sine = sin(thetas*pi/180))
+#	cosine = cos(thetas*pi/180)
+#	r = transpose(edge_image_flat*x)*cosines + transpose(edge_image_array*y)*sines
+#	Hough = zeros((len(thetas),len(r)),dtype=float)
+#	for x in thetas:
+#			Hough[x][round(r[:][x])] = Hough[x][round(r[:][x])]+1
+
