051a77234262fda05dcb02bb47063a611034720e
Tarun Yadav <tarunyadav.iitp@gmail.com>
2012-04-18 22:19:43 +0530

    Assingment 12 code for dilation,erosion,open,close is written

--- Assignment_12/morpho_operators.py
+++ Assignment_12/morpho_operators.py
@@ -21,28 +21,90 @@ from pylab import *
 #Function to apply dilation on a given image
 #@parameters: image and structure element
 #@return: image after applying dialtion to it
-def Dilation(Image, St_element,origin):
-	size= Image.shape;
+def Dilation(Image, St_element):
+	size= Image.shape
+	st_size=St_element.shape
+	N = st_size[0]
+	M=st_size[1]
+	Image_dialted= zeros((size[0],size[1]),dtype=int);
+	mask = (St_element==1)
 	
-	return Image
+	for i in range(0,size[0],1):
+			 for j in range(0,size[1],1):
+			 		if(Image[i][j]==1):
+					    		if(i in range(0,(N/2)) or  i in range (size[0]-(N/2), size[0]) or j in range(0,(M/2)) or j in range(size[1]-(M/2),size[1])):
+					    				start_row = max(-(N/2),0-i)
+					    				end_row = min((N/2),size[0]-1-i)
+					    				start_column = max(-(M/2),0-j)
+					    				end_column = min((M/2),size[1]-1-j)
+					    		else : 
+									start_row=-(N/2)
+									end_row=(N/2)
+									start_column=-(M/2)
+									end_column=(M/2)
+									
+							Window=Image[i+start_row:i+end_row][j+start_column:j+end_column]
+							window[N/2][m/2]=0
+							Window[mask]=1
+							Image_dilated[i+start_row:i+end_row][j+start_column:j+end_column] = Window
+										
+	return Image_dilated
 		
 #Function to apply erosion on a given image
 #@parameters: image and structure element
 #@return: image after applying erosion to it		
 def Erosion(Image, St_element):
-
-	return Image
+	size= Image.shape
+	st_size=St_element.shape
+	N = st_size[0]
+	M=st_size[1]
+	Image_eroded= zeros((size[0],size[1]),dtype=int);
+	mask = (St_element==1)
+	
+	for i in range(0,size[0],1):
+			 for j in range(0,size[1],1):
+			 		if(Image[i][j]==1):
+					    		if(i in range(0,(N/2)) or  i in range (size[0]-(N/2), size[0]) or j in range(0,(M/2)) or j in range(size[1]-(M/2),size[1])):
+					    				start_row = max(-(N/2),0-i)
+					    				end_row = min((N/2),size[0]-1-i)
+					    				start_column = max(-(M/2),0-j)
+					    				end_column = min((M/2),size[1]-1-j)
+					    		else : 
+									start_row=-(N/2)
+									end_row=(N/2)
+									start_column=-(M/2)
+									end_column=(M/2)
+									
+							Window=Image[i+start_row:i+end_row][j+start_column:j+end_column]
+							Window_mask = (Window==1)
+							
+							if (all(mask==Window_mask)):
+								Image_eroded[i][j] = 1
+							else:
+								Image_eroded[i][j] = 0
+										
+	return Image_dilated
 
 #Function to apply open on a given image
 #@parameters: image and structure element
 #@return: image after applying open to it	
 def Open(Image, St_element):
-
-	return Image
+	Image_eroded=Erosion(Image_dilated,St_element)
+	Image_opened=Dilation(Image_eroded,St_element)
+	return Image_opened
 
 #Function to apply close on a given image
 #@parameters: image and structure element
 #@return: image after applying close to it	
 def Close(Image, St_element):
-
-	return Image
+	Image_dilated=Dilation(Image,St_element)
+	Image_closed=Erosion(Image_dilated,St_element)
+	return Image_closed
+	
+#Function to find skeleton for a given image
+#@parameters: image and structure element
+#@return: Skeleton of the image
+def Close(Image, St_element):
+	Image_dilated=Dilation(Image,St_element)
+	Image_closed=Erosion(Image_dilated,St_element)
+	return Image_closed
--- Assignment_12/skeleton.py
+++ Assignment_12/skeleton.py
@@ -9,3 +9,113 @@
  #          Script to find skeleton of an image using  dilation,erosion,open,close operaotrs.
  #          Image Dimensions (WxH) - width(column) x height(row)
 ##
+
+#Basic import for python imaging 
+from os import *
+from sys import *
+from scipy import *
+from numpy import *
+from Image import *
+from pylab import *
+from time import *
+import string
+sys.setrecursionlimit(2000)
+## import all functions of morphological operators defined in the function file
+from morpho_operators.py import *
+
+def init():
+	global image
+	global image_array
+	global size
+	global image_output
+
+	image = open(sys.argv[1])
+	image = image.convert('L')
+	#image=image.filter(BLUR)
+	image_array = array(image)
+	size = image_array.shape
+	image_array = array(image_array,dtype=float)
+	image_output = zeros((size[0],size[1]),dtype=float)
+	
+#Save the output file
+def SaveToFile(output_name,t_init,region_no,normalized_chain_codes,area):
+	global image_output			
+	
+	t_end = time()
+	
+	file_name= sys.argv[1].split('/')
+	# check if output directory exists or not. if doesn't exists then create it
+	if (os.path.exists(os.path.dirname(file_name[0]+'_region_area/'))==False):
+	    print "\nCreating Directory "+file_name[0]+"_region_area ... Done"
+	    os.mkdir(file_name[0]+'_region_area',0777) 
+	else:
+	    print "\nOutput Directory "+file_name[0]+"_region_area already exist... Done"		
+
+	#convert array from float to uint8 for image display
+	image_output = image_output.astype('uint8')
+					
+	# convert from array to image
+	image_mod=fromarray(image_output);
+	print "Saving the region growing and boundary applied image to output directory..."
+	output_name = output_name + '_total_regions_'+str(region_no)+'_time_taken_' + str(t_end-t_init)+'_sec'
+	# save the image in destination folder	
+	image_mod.save(file_name[0]+'_region_area/'+file_name[1].split('.')[0]+output_name+'.gif')
+	print "Output image is saved in output Directory with name: "+ file_name[1].split('.')[0]+output_name+'.gif   ...Done\n'
+	
+	fd= os.open(file_name[0]+'_region_area/'+file_name[1].split('.')[0]+output_name+'.txt',os.O_RDWR|os.O_CREAT )
+	os.write(fd,'Area of the regions are: \n')
+	os.write(fd,str(area))
+	os.write(fd,str('\n\n'))
+	#os.write(fd,'Derivated Chain Codes: \n')
+	#os.write(fd,str(derivated_chain_codes))
+	os.close(fd)
+	print "----------------------------------------------------------------------------------------------------------------"
+	print ("\nTotal no. of regions are  : "+ str(region_no))	
+	print ("\nTotal time taken to run the Pogram is : "+str(t_end-t_init)+" seconds")
+	for i in range(1,region_no+1):
+		print ("\n Area of region : "+str(i)+" is: "+str(area[i]))
+	print "----------------------------------------------------------------------------------------------------------------"
+	import matplotlib.pyplot as plt
+	plt.imshow(image_output)
+	plt.show()
+	
+	#image_mod.show()
+
+# input for neighbor approach, gaussian smoothing and tolerance value for region detection
+def user_input_fun():
+	operator = raw_input("\n To apply Dilation Operator Press 1:  \n To apply Erosion Operator Press 2: \n To apply Open Operator Press 3: \n To apply Close Operator Press 4: \n To find Skeleton Press 5: \n Default is Skeleton [Press Enter]: ")
+
+	if(operator==""):
+		operator = 5
+	elif(operator !="1" && operator !="2" && operator !="3" && operator !="4"):
+		print(" Please choose the one of the followings: ")
+		user_input_function();
+			
+	return int(operator)
+			
+				
+#main function which will be run 
+def main():		
+	t_init= time()
+	# Calling the user input function to get maximum no. of regions and tolerance
+	operator = user_input_fun()
+	# initialization of all variables and arrays
+	init()
+	global image_array
+	if (operator==1):
+		Dilation(image_array,)
+	elif (operator==2):
+		Erosion(image_array,)
+	elif (operator==3):
+		Open(image_array,)
+	elif (operator==4):	
+		Close(image_array,)
+	elif (operator==5):
+		Skeleton(image_array,)
+	# in case to rerun the program		
+	rerun = raw_input("\nPress 'q' to quit  : \n"+"Press Enter to run again: \n")
+	if(rerun==""):
+		main()
+		
+main()
+	
